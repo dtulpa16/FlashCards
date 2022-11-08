9ad4cba8f4d7fc95c8803057411d971d19bb364c
Daniel Tulpa <dtulpa16@yahoo.com>
2022-11-08 14:18:32 -0600

    Added TailwindCSS Configuration + Createed Base Components

--- Flash Cards React/src/App.js
+++ Flash Cards React/src/App.js
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-
-
   return (
     <div>
-
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
---
+++ Flash Cards React/src/Components/Cards/CardCollection.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CardCollection(props) {
+  return (
+    <div>CardCollection</div>
+  )
+}
