a1afca7d918baebd65da940fa41340fd0fef52a1
Daniel Tulpa <dtulpa16@yahoo.com>
2022-11-09 13:42:21 -0600

    Updated SideBar Styles. Removed gap iIn Between Tabs

--- Flash Cards React/src/Components/SideBar/SideBar.jsx
+++ Flash Cards React/src/Components/SideBar/SideBar.jsx
@@ -6,12 +6,12 @@ export default function SideBar(props) {
   );
   return (
     <div>
-      <div class="flex flex-col gap-y-12  ">
+      <div class="flex flex-col ">
         {data &&
           data.map((el) => {
             return (
-              <div class="p-5 hover:bg-slate-50 transition delay-100 duration-300 ease-in-out">
-                <h3 class="pl-4">{el.title}</h3>
+              <div class="p-5 hover:bg-slate-50 transition delay-75 duration-300 ease-in-out">
+                <h3 class="pl-4 text-lg font-mono">{el.title}</h3>
               </div>
             );
           })}
